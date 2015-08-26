0b1fb2cfd2f76ff0e2af0b4c24d5ba0600dd1f53
wangjianlong <810844480@qq.com>
2015-08-26 12:10:43 +0800

    修改

--- LoowooTech.Jurisdiction.Web/LoowooTech.Jurisdiction.Web/Controllers/SafeController.cs
+++ LoowooTech.Jurisdiction.Web/LoowooTech.Jurisdiction.Web/Controllers/SafeController.cs
@@ -32,27 +32,5 @@ namespace LoowooTech.Jurisdiction.Web.Controllers
             }
             return Redirect("/Home/Index");
         }
-
-
-        public ActionResult Message()
-        {
-            //待审批列表
-            ViewBag.List = Core.DataBookManager.GetWait(Identity);
-            //审批列表
-            ViewBag.Finishs = Core.DataBookManager.GetFinish(Identity.Name);
-            //我的申请
-            ViewBag.Mines = Core.DataBookManager.GetMine(Identity.Name);
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Message(int ID,string Reason,bool? Check)
-        {
-            Core.DataBookManager.Check(ID, Reason, Identity.Name, Check);
-            ViewBag.List = Core.DataBookManager.GetWait(Identity);
-            ViewBag.Finishs = Core.DataBookManager.GetFinish(Identity.Name);
-            ViewBag.Mines = Core.DataBookManager.GetMine(Identity.Name);
-            return View();
-        }
     }
 }
